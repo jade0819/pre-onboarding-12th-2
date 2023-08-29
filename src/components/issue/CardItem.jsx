3c6e508367d74d5bb8050d2566dcad4d4f6c0263
jade0819 <break0819@gmail.com>
2023-08-30 01:48:10 +0900

    Refactor: 재사용 가능한 CardItem UI 구현

---
+++ src/components/issue/CardItem.jsx
@@ -0,0 +1,28 @@
+import Text from '../ui/Text';
+
+const CardItem = ({ issue, handleClickTitle }) => {
+  const { number, title, user, created_at, comments } = issue;
+  const userInfo = `작성자: ${user.login}, 작성일: ${created_at}`;
+
+  return (
+    <>
+      <div className="basis-3/4 p-3">
+        <div>
+          <Text text={`#${number}`} />
+          <span
+            className="font-semibold ml-3 my-1 py-1 cursor-pointer"
+            onClick={handleClickTitle && handleClickTitle}
+          >
+            {title}
+          </span>
+        </div>
+        <Text className="text-xs" text={userInfo} />
+      </div>
+      <div className="basis-1/4 flex justify-end items-center px-3">
+        <Text text={`코멘트: ${comments}`} />
+      </div>
+    </>
+  );
+};
+
+export default CardItem;
--- src/components/issue/IssueItem.jsx
+++ src/components/issue/IssueItem.jsx
@@ -1,34 +1,18 @@
 import { useNavigate } from 'react-router';
 import Card from '../ui/Card';
-import Text from '../ui/Text';
+import CardItem from './CardItem';
 
-const IssueItem = ({ issue: { number, title, user, created_at, comments } }) => {
+const IssueItem = ({ issue }) => {
   const navigate = useNavigate();
 
-  const userInfo = `작성자: ${user.login}, 작성일: ${created_at}`;
-
   const handleClickTitle = () => {
-    navigate(`/issues/${number}`);
+    navigate(`/issues/${issue.number}`);
   };
 
   return (
     <li className="list-none">
       <Card>
-        <div className="basis-3/4 p-3">
-          <div>
-            <Text text={`#${number}`} />
-            <span
-              className="font-semibold ml-3 my-1 py-1 cursor-pointer"
-              onClick={handleClickTitle}
-            >
-              {title}
-            </span>
-          </div>
-          <Text className="text-xs" text={userInfo} />
-        </div>
-        <div className="basis-1/4 flex justify-end items-center px-3">
-          <Text text={`코멘트: ${comments}`} />
-        </div>
+        <CardItem issue={issue} handleClickTitle={handleClickTitle} />
       </Card>
     </li>
   );
