da23fad19119c9491aa9645701ff688ecd016149
jade0819 <break0819@gmail.com>
2023-08-30 11:59:38 +0900

    Feat: 코드를 깔끔하게 정리하기 위해 상수 추가

--- src/components/issue/IssueItem.jsx
+++ src/components/issue/IssueItem.jsx
@@ -1,13 +1,11 @@
 import { useNavigate } from 'react-router';
 import Card from '../ui/Card';
 import CardItem from '../ui/CardItem';
+import { AD_IMAGE_URL, WANTED_URL } from '../../constants/issuesData';
 
 const IssueItem = ({ mesureRef, issue, index }) => {
   const navigate = useNavigate();
   const fifthIndexFlag = index % 4 === 0 && index !== 0 ? true : false;
-  const adImageUrl =
-    'https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100';
-  const wantedUrl = 'https://www.wanted.co.kr/jobsfeed';
 
   const handleClickTitle = () => {
     const id = issue.number;
@@ -18,10 +16,10 @@ const IssueItem = ({ mesureRef, issue, index }) => {
     <li className="list-none" ref={mesureRef}>
       {fifthIndexFlag && (
         <div
-          onClick={() => window.open(wantedUrl)}
+          onClick={() => window.open(WANTED_URL)}
           className="h-[97px] flex justify-center items-center border-b border-b-gray-300 cursor-pointer"
         >
-          <img src={adImageUrl} alt="AD wanted image" />
+          <img src={AD_IMAGE_URL} alt="AD wanted image" />
         </div>
       )}
       <Card>{issue && <CardItem issue={issue} handleClickTitle={handleClickTitle} />}</Card>
---
+++ src/constants/issuesData.js
@@ -0,0 +1,3 @@
+export const WANTED_URL = 'https://www.wanted.co.kr/jobsfeed';
+export const AD_IMAGE_URL =
+  'https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100';
