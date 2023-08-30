fbc007422afb47ada13ac8d400705eba6eea7914
jade0819 <break0819@gmail.com>
2023-08-30 13:32:33 +0900

    Refactor: Organization Name과 Repository Name 상수로 관리

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import { OWNER, REPO } from '../constants/issuesData';
 
 const Header = () => {
   return (
     <div className="w-full h-24 flex justify-center items-center bg-brand-gray border-b border-b-gray-200">
       <Link to="/" className="text-brand no-underline cursor-pointer">
-        <h1>Facebook / react</h1>
+        <h1>
+          {OWNER} / {REPO}
+        </h1>
       </Link>
     </div>
   );
