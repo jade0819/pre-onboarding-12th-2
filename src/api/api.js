fe39d412b88eb5c9298d7aaed2561bef99ee14a6
jade0819 <break0819@gmail.com>
2023-08-31 01:33:27 +0900

    Fix: 이슈 리스트 생성 순이 아닌 코멘트 순으로 변경 및 불 필요한 파일 삭제

--- src/api/api.js
+++ src/api/api.js
@@ -1,25 +1,15 @@
-// import api from './axios';
-
-// export const getIssues = async page => {
-//   const response = await api.get(`/issues?per_page=10&page=${page}&state=open&sort=comments`);
-//   return response.data;
-// };
-
-// export const getIssueItem = async id => {
-//   const response = await api.get(`/issues/${id}`);
-//   return response.data;
-// };
-
 import { Octokit } from 'octokit';
 
 const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN,
 });
 
-export const getIssues = async () => {
+export const getIssues = async option => {
   const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
     owner: 'facebook',
     repo: 'react',
+    state: 'open',
+    sort: 'comments',
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
     },
