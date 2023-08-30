911c27fe62b2f01563cce77a10479adecffd1291
jade0819 <break0819@gmail.com>
2023-08-30 13:57:36 +0900

    Refactor: api 로직 변경 (axios를 사용한 REST API 로직 -> octokit 라이브러리를 사용한 api 로직)

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,46 @@
-import api from './axios';
+// import api from './axios';
 
-export const getIssues = async page => {
-  const response = await api.get(`/issues?per_page=10&page=${page}&state=open&sort=comments`);
+// export const getIssues = async page => {
+//   const response = await api.get(`/issues?per_page=10&page=${page}&state=open&sort=comments`);
+//   return response.data;
+// };
+
+// export const getIssueItem = async id => {
+//   const response = await api.get(`/issues/${id}`);
+//   return response.data;
+// };
+
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_GITHUB_TOKEN,
+});
+
+export const getIssues = async () => {
+  const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+    owner: 'facebook',
+    repo: 'react',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+
   return response.data;
 };
 
 export const getIssueItem = async id => {
-  const response = await api.get(`/issues/${id}`);
+  const octokit = new Octokit({
+    auth: process.env.REACT_APP_GITHUB_TOKEN,
+  });
+
+  const response = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner: 'facebook',
+    repo: 'react',
+    issue_number: id,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+
   return response.data;
 };
