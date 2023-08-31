9e167e4227a752c23e3d7c4bfbfed35e74abe5bb
jade0819 <break0819@gmail.com>
2023-09-01 00:54:50 +0900

    Docs: README 파일 내용 수정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,8 @@ import { OWNER, REPO } from '../constants/issuesData';
 const Header = () => {
   return (
     <div className="w-full h-24 flex justify-center items-center bg-black border-b border-b-gray-200">
-      <Link to="/" className="text-white no-underline cursor-pointer">
+      {/* <Link to="/" className="text-white no-underline cursor-pointer"> */}
+      <Link to="/" className="text-3xl text-white no-underline cursor-pointer">
         <h1>
           {OWNER} / {REPO}
         </h1>
